473c5d98f633e0f784fd01fd1790f53d96954e1d
devil876 <pankaj2000choudhary@gmail.com>
2026-07-22 11:29:58 +0530

    Fix iframe JSX attributes

--- app/page.tsx
+++ app/page.tsx
@@ -367,14 +367,13 @@ export default function Home() {
     <div className="mt-14 overflow-hidden rounded-[2rem] shadow-xl border border-[var(--color-cream-deep)]">
 
       <iframe
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4586.905014835202!2d76.15953675197734!3d32.22540345024845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391b5f000fb56067%3A0x6cdf2e85ebcd9ad!2sRustic%20Roots%20stay!5e1!3m2!1sen!2sin!4v1784532468683!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-c"
-  width="100%"
-  height="500"
+  src="..."
+  width="600"
+  height="450"
   style={{ border: 0 }}
   allowFullScreen
   loading="lazy"
   referrerPolicy="no-referrer-when-downgrade"
-  className="w-full"
 />
 
     </div>
